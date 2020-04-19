443e6e232ccee8b28afc489b5883cc2377d7edd4
Jijo Bose <bosejijo@gmail.com>
2020-04-19 20:04:20 +0530

    Address in User Model

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,20 +1,41 @@
 class User {
   int id;
   String name;
+  String username;
   String email;
+  var address;
 
-  User(int id, String name, String email) {
+  User(int id, String name, String username, String email, var address) {
     this.id = id;
     this.name = name;
+    this.username = username;
     this.email = email;
+    this.address = {
+      'street': address.street,
+      'suite': address.suite,
+      'city': address.city,
+      'zipcode': address.zipcode,
+      'geo': {
+        'lat': address.geo.lat,
+        'lng': address.geo.lng,
+      }
+    };
   }
 
   User.fromJson(Map json)
       : id = json['id'],
         name = json['name'],
-        email = json['email'];
+        username = json['username'],
+        email = json['email'],
+        address = json['address'];
 
   Map toJson() {
-    return {'id': id, 'name': name, 'email': email};
+    return {
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'address': address
+    };
   }
 }
